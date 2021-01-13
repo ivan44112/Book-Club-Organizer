327ff7c7c0285744aa31a9ac727e62eb0651a2c6
Filip Degmecic <filip_degmecic@yahoo.com>
2021-01-13 14:41:40 +0100

    login feature

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -28,5 +28,30 @@ router.post("/register", async(req,res)=>{
     }
 });
 
+router.post("/login", async(req,res)=>{
+
+    const{email,password}=req.body;
+
+    try {
+        const user = await pool.query("SELECT * FROM users WHERE user_email=$1",[email]);
+
+        if(user.rows.lenght===0){
+            return res.status(401).json("Password or Email is incorrect");
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+        if(!validPassword){
+            return res.status(401).json("Password or Email is incorrect");
+        }
+
+        const token = jwtGenerator(user.rows[0].user_id);
+
+        res.json({token});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 
 module.exports = router;
